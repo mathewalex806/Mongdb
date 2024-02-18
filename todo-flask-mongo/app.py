b3ab96293f963126ce5702151d78c883ed74d430
Alex <mathewalex806@gmail.com>
2024-02-18 10:13:42 +0530

    basic todo app

--- todo-flask-mongo/app.py
+++ todo-flask-mongo/app.py
@@ -8,13 +8,21 @@ client = MongoClient()
 db = client["flask-database"]
 todos = db["todos"]
 
+list_item =[]
 
 @app.route("/", methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        item = request.form["list_item"]
 
+        if item not in list_item:
+            insert_item = { "item": item,}
+            todos.insert_one(insert_item)
+            list_item.append(item)
 
-
+        return render_template('index.html',list_items = todos.find())
+    
+    return render_template('index.html',list_items = todos.find())
 
 if __name__ == "__main__":
     app.run(debug=True)
